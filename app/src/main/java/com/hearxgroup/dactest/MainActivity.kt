118b22e99364689cd1b78a6656e421b9d62ac82f
davidhowe <umbredave@gmail.com>
2019-10-14 10:46:14 +0200

    - Working i2c commands paired with CP2615

---
+++ app/src/main/java/com/hearxgroup/dactest/MainActivity.kt
@@ -0,0 +1,153 @@
+package com.hearxgroup.dactest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import android.widget.Toast
+import android.content.*
+import android.os.Handler
+import android.widget.EditText
+import android.widget.TextView
+import android.os.IBinder
+import android.os.Message
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ref.WeakReference
+
+class MainActivity : AppCompatActivity() {
+
+    /*
+     * Notifications from UsbService will be received here.
+     */
+    private val mUsbReceiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            when (intent.action) {
+                UsbService.ACTION_USB_PERMISSION_GRANTED // USB PERMISSION GRANTED
+                -> Toast.makeText(context, "USB Ready", Toast.LENGTH_SHORT).show()
+                UsbService.ACTION_USB_PERMISSION_NOT_GRANTED // USB PERMISSION NOT GRANTED
+                -> Toast.makeText(context, "USB Permission not granted", Toast.LENGTH_SHORT).show()
+                UsbService.ACTION_NO_USB // NO USB CONNECTED
+                -> Toast.makeText(context, "No USB connected", Toast.LENGTH_SHORT).show()
+                UsbService.ACTION_USB_DISCONNECTED // USB DISCONNECTED
+                -> Toast.makeText(context, "USB disconnected", Toast.LENGTH_SHORT).show()
+                UsbService.ACTION_USB_NOT_SUPPORTED // USB NOT SUPPORTED
+                -> Toast.makeText(context, "USB device not supported", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    private var usbService: UsbService? = null
+    private var display: TextView? = null
+    private var editText: EditText? = null
+    private var mHandler: MyHandler? = null
+    private val usbConnection = object : ServiceConnection {
+        override fun onServiceConnected(arg0: ComponentName, arg1: IBinder) {
+            usbService = (arg1 as UsbService.UsbBinder).service
+            usbService!!.setHandler(mHandler)
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            usbService = null
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mHandler = MyHandler(this)
+
+        display = textView1
+        editText = editText1
+
+        buttonRed.setOnClickListener {
+            if (usbService != null) { // if UsbService was correctly binded, Send data
+                usbService!!.writeRed()
+            }
+        }
+
+        buttonGreen.setOnClickListener {
+            if (usbService != null) { // if UsbService was correctly binded, Send data
+                usbService!!.writeGreen()
+            }
+        }
+
+        buttonBlue.setOnClickListener {
+            if (usbService != null) { // if UsbService was correctly binded, Send data
+                usbService!!.writeBlue()
+            }
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        setFilters()  // Start listening notifications from UsbService
+        startService(
+            UsbService::class.java,
+            usbConnection,
+            null
+        ) // Start UsbService(if it was not started before) and Bind it
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        unregisterReceiver(mUsbReceiver)
+        unbindService(usbConnection)
+    }
+
+    private fun startService(
+        service: Class<*>,
+        serviceConnection: ServiceConnection,
+        extras: Bundle?
+    ) {
+        if (!UsbService.SERVICE_CONNECTED) {
+            val startService = Intent(this, service)
+            if (extras != null && !extras.isEmpty) {
+                val keys = extras.keySet()
+                for (key in keys) {
+                    val extra = extras.getString(key)
+                    startService.putExtra(key, extra)
+                }
+            }
+            startService(startService)
+        }
+        val bindingIntent = Intent(this, service)
+        bindService(bindingIntent, serviceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    private fun setFilters() {
+        val filter = IntentFilter()
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_GRANTED)
+        filter.addAction(UsbService.ACTION_NO_USB)
+        filter.addAction(UsbService.ACTION_USB_DISCONNECTED)
+        filter.addAction(UsbService.ACTION_USB_NOT_SUPPORTED)
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_NOT_GRANTED)
+        registerReceiver(mUsbReceiver, filter)
+    }
+
+    /*
+     * This handler will be passed to UsbService. Data received from serial port is displayed through this handler
+     */
+    private class MyHandler(activity: MainActivity) : Handler() {
+        private val mActivity: WeakReference<MainActivity> = WeakReference(activity)
+
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                UsbService.MESSAGE_FROM_SERIAL_PORT -> {
+                    val data = msg.obj as String
+                    mActivity.get()?.display?.append(data)
+                }
+                UsbService.CTS_CHANGE -> Toast.makeText(
+                    mActivity.get(),
+                    "CTS_CHANGE",
+                    Toast.LENGTH_LONG
+                ).show()
+                UsbService.DSR_CHANGE -> Toast.makeText(
+                    mActivity.get(),
+                    "DSR_CHANGE",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+}
